3ddb83a7b20b00993d8d469b8a3e44269fe51066
Joe DeKeyrel <joedekeyrel@gmail.com>
2023-08-12 13:19:54 -0400

    Fixed refresh show argument; going for fast update

--- clientdisplay.py
+++ clientdisplay.py
@@ -122,7 +122,8 @@ class Matrix(object):
         parser.add_argument("-b", "--led-brightness", action="store", help="Sets brightness level. Default: 100. Range: 1..100", default=100, type=int)
         parser.add_argument("-m", "--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm" , default='adafruit-hat', choices=['regular', 'regular-pi1', 'adafruit-hat', 'adafruit-hat-pwm'], type=str)
         parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="U-mapper", type=str)
-        parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
+        parser.add_argument("--led-show-refresh", action="store", help="Shows the current refresh rate of the LED panel")
+        # parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
         parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 4", default=4, type=int) # 2 works pretty well for a pi3A+, 4 for a pi4b
         parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
         parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
@@ -237,7 +238,7 @@ class Matrix(object):
             anow = arrow.now().to('US/Eastern')
             # check for redis-delivered command
             self.check_redis_command()
-            # gove the displays a chance to update their data
+            # give the displays a chance to update their data
             for d in self.displays:
                  d[0].check(anow)
             if lastds == 0 or now - lastds > dsint:
@@ -247,16 +248,19 @@ class Matrix(object):
                 fsint = dsint / framect
                 frame = 0
                 lastds = lastfs = now
-                offscreen_canvas = self.displays[self.display][0].display()
-                if matrix is not None:
-                    offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+                # offscreen_canvas = self.displays[self.display][0].display()
+                # if matrix is not None:
+                #     offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
         #         print("Display: {}, Frame {} of {}".format(type(displays[display][0]).__name__,frame+1,framect))
             elif lastfs == 0 or now - lastfs > fsint:
                 frame = (frame + 1) % framect
                 lastfs = now
-                offscreen_canvas = self.displays[self.display][0].display()
-                if matrix is not None:
-                    offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+                # offscreen_canvas = self.displays[self.display][0].display()
+                # if matrix is not None:
+                #     offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+            offscreen_canvas = self.displays[self.display][0].display()
+            if matrix is not None:
+                offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
             time.sleep(0.1)
 
 m = Matrix()
