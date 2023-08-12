38c3da8309ef39a50a844a460f4b0a62f4d5c752
dekeyrej <74379607+dekeyrej@users.noreply.github.com>
2023-08-12 14:17:22 -0400

    Update mc.sh

--- mc.sh
+++ mc.sh
@@ -2,6 +2,6 @@
 cd /home/pi/matrixclient
 # cd /home/pi/kubematrix/client
 source venv/bin/activate
-/home/pi/matrixclient/clientdisplay.py -i 1
+/home/pi/matrixclient/venv/bin/python /home/pi/matrixclient/clientdisplay.py -i 1
 # /home/pi/kubematrix/client/clientdisplay.py -i 1
 #/home/pi/matrix/clientdisplay.py
